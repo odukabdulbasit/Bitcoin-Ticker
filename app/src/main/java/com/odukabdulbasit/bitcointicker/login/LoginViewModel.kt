981a1049d000b29b1230388573bbb018a1228080
Abdulbasit <2016212056@cumhuriyet.edu.tr>
2021-08-19 17:03:32 +0300

    Added ViewModelFactory use used it

--- app/src/main/java/com/odukabdulbasit/bitcointicker/login/LoginViewModel.kt
+++ app/src/main/java/com/odukabdulbasit/bitcointicker/login/LoginViewModel.kt
@@ -1,13 +1,55 @@
 package com.odukabdulbasit.bitcointicker.login
 
+import android.app.Application
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(val application: Application) : ViewModel() {
 
-
+    private val firebaseAuth : FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
 
     //login to firebase current user with email and password
-    fun login(){
+    fun login(email: String , password: String){
+        if (!email.isNullOrEmpty() && !password.isNullOrEmpty()){
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(application, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        else {
+            Toast.makeText(application, "Lütfen gerekli alanları doldurunuz", Toast.LENGTH_LONG).show()
+        }
 
     }
+
+
+    /**
+     * Factory for constructing DevByteViewModel with parameter
+     */
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return LoginViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
+
+
+
