271e13a4e9daa8073ede1df989bf9069ca7a0a11
Abdulbasit <2016212056@cumhuriyet.edu.tr>
2021-08-19 15:00:06 +0300

    Created LoginView.kt

--- app/src/main/java/com/odukabdulbasit/bitcointicker/login/LoginViewModel.kt
+++ app/src/main/java/com/odukabdulbasit/bitcointicker/login/LoginViewModel.kt
@@ -3,4 +3,11 @@ package com.odukabdulbasit.bitcointicker.login
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+
+
+
+    //login to firebase current user with email and password
+    fun login(){
+
+    }
 }
